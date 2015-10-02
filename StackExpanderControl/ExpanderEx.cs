61c9c43b945258fdd244e39282ef16eb2aa0e162
sagamors <sagamor@inbox.ru>
2015-10-02 23:23:19 +0300

    begin realization

---
+++ StackExpanderControl/ExpanderEx.cs
@@ -0,0 +1,102 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace StackExpanderControl
+{
+    class ExpanderEx : Expander
+    {
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            ChangePath("Header", DisplayHeaderPath);
+
+        }
+
+        public static readonly DependencyProperty DisplayHeaderPathProperty = DependencyProperty.Register(
+            "DisplayHeaderPath", typeof(string), typeof(ExpanderEx), new PropertyMetadata(default(string),
+                (o, args) =>
+                {
+                    var control = (ExpanderEx)o;
+                    control.ChangePath("Header", control.DisplayHeaderPath);
+                }));
+
+        public string DisplayHeaderPath
+        {
+            get { return (string)GetValue(DisplayHeaderPathProperty); }
+            set { SetValue(DisplayHeaderPathProperty, value); }
+        }
+
+        private void ChangePath(string NameProperty, string newPath)
+        {
+            var descriptor = DependencyPropertyDescriptor.FromName(NameProperty, GetType(), GetType());
+            var bindingExpression = this.GetBindingExpression(descriptor.DependencyProperty);
+            if (bindingExpression == null) return;
+            var _binding = bindingExpression.ParentBinding;
+            SetBinding(descriptor.DependencyProperty, _binding.Clone("DataContext." + newPath));
+        }
+
+        public static readonly DependencyProperty DisplayMemberPathProperty = DependencyProperty.Register(
+            "DisplayMemberPath", typeof(string), typeof(ExpanderEx), new PropertyMetadata(default(string),
+                (o, args) =>
+                {
+                    var control = (ExpanderEx)o;
+                    control.ChangePath("Content", control.DisplayMemberPath);
+                }));
+
+        public string DisplayMemberPath
+        {
+            get { return (string)GetValue(DisplayMemberPathProperty); }
+            set { SetValue(DisplayMemberPathProperty, value); }
+        }
+    }
+
+    public static class BindingEx
+    {
+        public static Binding Clone(this Binding self)
+        {
+            return self.Clone(self.Path.Path);
+        }
+
+        public static Binding Clone(this Binding self, string path)
+        {
+            Binding clone = new Binding(path);
+            if (self.Source != null)
+                clone.Source = self.Source;
+            clone.AsyncState = self.AsyncState;
+            clone.BindsDirectlyToSource = self.BindsDirectlyToSource;
+            clone.Converter = self.Converter;
+            clone.ConverterParameter = self.ConverterParameter;
+            if (self.ElementName != null)
+                clone.ElementName = self.ElementName;
+            clone.ConverterCulture = self.ConverterCulture;
+            clone.IsAsync = clone.IsAsync;
+            clone.NotifyOnSourceUpdated = self.NotifyOnSourceUpdated;
+            clone.NotifyOnTargetUpdated = self.NotifyOnTargetUpdated;
+            clone.Mode = self.Mode;
+            clone.NotifyOnValidationError = self.NotifyOnValidationError;
+            clone.UpdateSourceTrigger = self.UpdateSourceTrigger;
+            clone.ValidatesOnDataErrors = self.ValidatesOnDataErrors;
+            clone.UpdateSourceExceptionFilter = self.UpdateSourceExceptionFilter;
+            clone.ValidatesOnNotifyDataErrors = self.ValidatesOnNotifyDataErrors;
+            clone.ValidatesOnExceptions = self.ValidatesOnExceptions;
+
+            foreach (var validationRule in self.ValidationRules)
+            {
+                clone.ValidationRules.Add(validationRule);
+            }
+
+            clone.BindingGroupName = self.BindingGroupName;
+            clone.StringFormat = self.StringFormat;
+            clone.FallbackValue = self.FallbackValue;
+            clone.Delay = self.Delay;
+            clone.RelativeSource = self.RelativeSource;
+            clone.XPath = self.XPath;
+            clone.TargetNullValue = self.TargetNullValue;
+
+            return clone;
+        }
+    }
+}
---
+++ StackExpanderControl/StackExpander.cs
@@ -0,0 +1,67 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace StackExpanderControl
+{
+    /// <summary>
+    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+    ///
+    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:StackExpanderControl"
+    ///
+    ///
+    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:StackExpanderControl;assembly=StackExpanderControl"
+    ///
+    /// You will also need to add a project reference from the project where the XAML file lives
+    /// to this project and Rebuild to avoid compilation errors:
+    ///
+    ///     Right click on the target project in the Solution Explorer and
+    ///     "Add Reference"->"Projects"->[Select this project]
+    ///
+    ///
+    /// Step 2)
+    /// Go ahead and use your control in the XAML file.
+    ///
+    ///     <MyNamespace:CustomControl1/>
+    ///
+    /// </summary>
+    public class StackExpander : ItemsControl
+    {
+        static StackExpander()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof (StackExpander),
+                new FrameworkPropertyMetadata(typeof (StackExpander)));
+        }
+
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            return item is StackExpanderItem;
+        }
+
+        protected override void PrepareContainerForItemOverride(DependencyObject element, object item)
+        {
+            base.PrepareContainerForItemOverride(element, item);
+
+            var contentPresenter = ((StackExpanderItem)element);
+            contentPresenter.ContentTemplate = ItemTemplate;
+            var framework = item as FrameworkElement;
+            if (framework != null && framework.Parent!=null)
+            {
+        /*        RemoveChildHelper.RemoveChild(framework.Parent, framework);*/
+                contentPresenter.Content = "rety";
+            }
+        }
+
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new StackExpanderItem();
+        }
+    }
+}
